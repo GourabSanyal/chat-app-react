c8f864be64060c514066eb359f2c83ba366fa9f0
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-08-29 22:02:49 +0530

    messeges updated

--- src/components/MessagewForm.jsx
+++ src/components/MessagewForm.jsx
@@ -1,9 +1,34 @@
-const MessagewForm = () => {
+import { useState } from "react";
+import { sendMessage, isTyping} from 'react-chat-engine';
+
+const MessagewForm = (props) => {
+    const [value, setValue] = useState('');
+    const handleSubmit = () => {
+        e.preventDefault();
+
+         const text = value.trim();
+
+         if(text.length > 0) sendMessage( creds, chatId, {text})
+    }
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+
+        isTyping(props, chatId)
+    }
+
     return (
-        <div>
-            <h3>MEssage Form</h3>            
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className="message-form"
+                placeholder="Send a message..."
+                value = {value}
+                onChange = {handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     )
 }
 
 export default MessagewForm
+ 
